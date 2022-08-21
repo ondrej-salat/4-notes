8bad28689ef5ca3eafacac7681015b7f8859435f
ondrej-salat <78552892+ondrej-salat@users.noreply.github.com>
2022-08-21 13:25:06 +0200

    user login/signup

--- 4-notes_server/database_data.py
+++ 4-notes_server/database_data.py
@@ -1,2 +1,45 @@
+from datetime import datetime
 import sqlite3
+import bcrypt
 
+
+def user_exists(user_name):
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    query = 'select exists(select 1 from user where user_name=?) limit 1'
+    check = cur.execute(query, (user_name,))
+    if check.fetchone()[0] == 0:
+        con.close()
+        return False
+    con.close()
+    return True
+
+
+def create_user(user, password, email):
+    if user_exists(user):
+        return False
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute(f'INSERT INTO user (user_name, password, email, created) VALUES (?, ?, ?, ?)',
+                (user, password, email, datetime.now().strftime('%d.%m.%Y %H:%M:%S')))
+    con.commit()
+    con.close()
+    return True
+
+
+def get_password(user_name):
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    cur.execute('SELECT * from user where user_name = ? ;', (user_name,))
+    password = cur.fetchall()[0][1]
+    con.close()
+    return password
+
+
+def get_email(user_name):
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    cur.execute('SELECT * from user where user_name = ? ;', (user_name,))
+    email = cur.fetchall()[0][2]
+    con.close()
+    return email
--- 4-notes_server/main.py
+++ 4-notes_server/main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI, Request, HTTPException
 import jwt
+import hashlib
+import secrets
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
+from database_data import *
 
 SECRET_KEY = "YOUR_FAST_API_SECRET_KEY"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRES_MINUTES = 800
+PEPER = "SOME_PEPER"
 
-test_user = {
-    "username": "user",
-    "password": "password",
-
-}
 app = FastAPI()
 
 origins = [
@@ -34,6 +33,30 @@ class LoginItem(BaseModel):
     password: str
 
 
+class SignupItem(BaseModel):
+    username: str
+    password: str
+    email: str
+
+
+def get_salt():
+    return secrets.token_hex(8)
+
+
+def hash_password(plain_text_password):
+    salt = get_salt()
+    password = (plain_text_password + salt + PEPER).encode('utf-8')
+    hashed_password = hashlib.sha512(password).hexdigest()
+    return f'{salt}${hashed_password}'
+
+
+def verify_password(plain_text_password, hashed_password):
+    salt = hashed_password.split("$")[0]
+    password = (plain_text_password + salt + PEPER).encode('utf-8')
+    new_hash = hashlib.sha512(password).hexdigest()
+    return f'{salt}${new_hash}' == hashed_password
+
+
 def get_token(req):
     token = req.headers["Authorization"].split()[1]
     return token
@@ -41,7 +64,7 @@ def get_token(req):
 
 def authorize_token(token):
     data = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-    if data['username'] == test_user['username'] and data['password'] == test_user["password"]:
+    if user_exists(data['username']):
         return True
     return False
 
@@ -54,11 +77,23 @@ async def root():
 @app.post("/login")
 async def user_login(login_item: LoginItem):
     data = jsonable_encoder(login_item)
-    if data['username'] == test_user['username'] and data['password'] == test_user['password']:
-        encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
-        return {"token": encoded_jwt}
+    if user_exists(data['username']):
+        if verify_password(data['password'], get_password(data['username'])):
+            payload = {'username': data['username'], 'email': get_email(data['username'])}
+            encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+            return {"token": encoded_jwt}
     else:
         return {"message": "login failed"}
+
+
+@app.post('/signup')
+async def user_signup(signup_item: SignupItem):
+    data = jsonable_encoder(signup_item)
+    if create_user(data['username'], hash_password(data['password']), data['email']):
+        payload = {'username': data['username'], 'email': data['email']}
+        encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+        return {"token": encoded_jwt}
+    return {"message": "Signin failed"}
 
 
 @app.get("/notes")
